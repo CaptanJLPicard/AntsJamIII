68a2c0e7c7b50539726c44f268906086802b95f5
DESKTOP-07DTHHO\MesutLap <mstklyciw@gmail.com>
2025-12-02 02:15:54 +0300

    SliderBitti

--- Assets/Scripts/BaseScripts.cs
+++ Assets/Scripts/BaseScripts.cs
@@ -1,5 +1,6 @@
 using MoreMountains.Feedbacks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseScripts : MonoBehaviour
 {
@@ -9,14 +10,27 @@ public class BaseScripts : MonoBehaviour
     [SerializeField] private GameObject WinMenu;
     [SerializeField] private bool isFriend;
 
+    [Header("UI")]
+    [SerializeField] private Slider baseHealthSlider;
+    private float maxBaseHealth;
+
     private void Start()
     {
         LoseMenu.SetActive(false);
         WinMenu.SetActive(false);
+
+        maxBaseHealth = baseHealth;
+        if (baseHealthSlider != null)
+        {
+            baseHealthSlider.maxValue = maxBaseHealth;
+            baseHealthSlider.value = baseHealth;
+        }
     }
+
     private void Update()
     {
         HealthCheck();
+        UpdateHealthUI();
     }
 
     private void HealthCheck()
@@ -27,13 +41,24 @@ public class BaseScripts : MonoBehaviour
             gameObject.layer = 0;
             Destroy(gameObject, 3f);
 
-            if(isFriend) LoseMenu.SetActive(true);
-            else WinMenu.SetActive(true);
+            if (isFriend)
+                LoseMenu.SetActive(true);
+            else
+                WinMenu.SetActive(true);
         }
     }
 
     public void BaseDamage(float damage)
     {
         baseHealth -= damage;
+        UpdateHealthUI();
+    }
+
+    private void UpdateHealthUI()
+    {
+        if (baseHealthSlider != null)
+        {
+            baseHealthSlider.value = baseHealth;
+        }
     }
 }
